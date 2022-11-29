312c08dfe61fc7cbf1c6ae14e322310c45f552a2
Miles Wada <wadam215@gmail.com>
2022-11-28 23:10:58 -0800

    initial commit

---
+++ cmake/build/CMakeFiles/hw_grpc_proto.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_grpc_proto.dir/stringreverse.grpc.pb.cc.o"
+  "CMakeFiles/hw_grpc_proto.dir/stringreverse.grpc.pb.cc.o.d"
+  "CMakeFiles/hw_grpc_proto.dir/stringreverse.pb.cc.o"
+  "CMakeFiles/hw_grpc_proto.dir/stringreverse.pb.cc.o.d"
+  "libhw_grpc_proto.a"
+  "libhw_grpc_proto.pdb"
+  "stringreverse.grpc.pb.cc"
+  "stringreverse.grpc.pb.h"
+  "stringreverse.pb.cc"
+  "stringreverse.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_grpc_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
